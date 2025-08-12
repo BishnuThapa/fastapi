5fd562fede5cde152b2d39bbb34c67e1aebb5c66
Bishnu <thapabishnu20@gmail.com>
2025-08-12 12:48:14 +0545

    fastapi path parameter

---
+++ ch4/app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to the FastAPI application!"}
+
+# Get request
+#fetch or read all the data
+@app.get("/product")
+async def all_products():
+    return {"message": "Fetching all products"}
+
+# fetch or read all single data
+@app.get("/product/{product_id}")
+async def single_product(product_id:int):
+    return {"message": "Single product fetched", "Product_id": product_id}
